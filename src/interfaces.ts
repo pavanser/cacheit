0cde9e9e127e1215f273da6b2db8d6ac74f80990
pavanser <pavanser@outlook.com>
2020-03-06 17:24:13 +0200

    Updated comments in interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,36 +5,51 @@ export interface AbstractCache {
 }
 
 export interface IStorageItems {
-  [key: string]: ICollection;
+  /**
+   * This is an object where key is a collectionName and value is a collection instance
+   * */
+  [collectionName: string]: ICollection;
 }
 
 export interface IStorage {
+  /**
+   * Getter which will return current storage collections as a snapshot.
+   * */
   readonly storage: IStorageItems;
   /**
-   * Method used for creating different storage types in depends from browser and its mode;
-   * It will return one of AbstractLocalStorageStore | AbstractSessionStorageStore | AbstractIndexedDBStore instance;
+   * Adding new collection [[ICollection]] to current storage by {@params collectionName}
    * */
-  addCollection(storeName: string): void;
+  addCollection(collectionName: string): void;
 
   /**
-   * Method used for removing current store instance
+   * Method used for removing collection [[ICollection]] from storage by {@params collectionName}
    * */
-  deleteCollection(): void;
+  deleteCollection(collectionName: string): void;
 }
 
+/**
+ * Success response for all collection methods
+ * */
 export interface IResult {
   status: Statuses;
   action: Actions;
   items: ICollectionItem[];
+  error?: Error;
 }
 
 export interface ICollectionItem {
+  /**
+   * Each collection item should have id, for controlling uniq of item in collection and manipulating of it.
+   * */
   id: string;
-  [key: string]: any;
+  /**
+   * Defined defined properties of collection items.
+   * */
+  [property: string]: any;
 }
 
 /**
- * Description of methods, what each store instance will return.
+ * Description of methods, what each collection instance will return.
  */
 export interface ICollection {
   /**
@@ -42,20 +57,20 @@ export interface ICollection {
    * */
   readonly name: string;
   /**
-   * Add single ICollectionItem in current store instance.
-   * Item id is required for proper adding and future usage.
+   * Add single collection item in current collection instance.
+   * Item's id is required for proper adding and future usage.
    * */
   add(item: ICollectionItem): Promise<IResult>;
 
   /**
-   * Bulk adding items in current store instance.
-   * Method expected ICollectionItem[] here.
+   * Bulk adding items in current collection instance.
+   * Method expected array of [collection items here.
    * Item id is required in each array item for proper adding and future usage.
    * */
   addBulk(items: ICollectionItem[]): Promise<IResult>;
 
   /**
-   * Delete all items from current store instance
+   * Delete all items from current collection instance
    * */
   clear(key: string): Promise<IResult>;
 
@@ -65,50 +80,50 @@ export interface ICollection {
   get(key: string): Promise<ICollectionItem>;
 
   /**
-   * Getting all items from current store instance
+   * Getting all items from current collection instance
    * */
   getAll(): Promise<ICollectionItem[]>;
 
   /**
-   * Delete items by {@params key} from current store instance
+   * Delete items by {@params key} from current collection instance
    * */
   delete(key: string): Promise<IResult>;
 
   /**
-   * Delete more than on item by {@params keys} from current store instance
+   * Delete more than on item by {@params keys} from current collection instance
    * */
   deleteBulk(...keys: string[]): Promise<IResult>;
 
   /**
-   * Get all current store instance keys
+   * Get all current collection instance keys
    * */
   keys(): Promise<string[]>;
 
   /**
-   * Update single ICollectionItem in current store instance.
+   * Update single collection item in current collection instance.
    * Could be full item or it's part.
    * Item id is required for proper updating.
    * */
   put(item: ICollectionItem): Promise<IResult>;
 
   /**
-   * Bulk updating items in current store instance.
-   * Method expected an ICollectionItem[]here.
+   * Bulk updating items in current collection instance.
+   * Method expected an array of collection items here.
    * Could be full item or it's part.
    * Item id is required for proper updating.
    * */
   putBulk(items: ICollectionItem[]): Promise<IResult>;
 
   /**
-   * Update or add ICollectionItem in current store instance.
+   * Update or add collection item in current collection instance.
    * Could be full item or it's part.
    * Item id is required for proper updating or adding.
    * */
   upsert(item: ICollectionItem): Promise<IResult>;
 
   /**
-   * Bulk updating or adding items in current store instance.
-   * Method expected an ICollectionItem[] here.
+   * Bulk updating or adding items in current collection instance.
+   * Method expected an array of collection items here.
    * Could be full item or it's part.
    * Item id is required for proper updating.
    * */
